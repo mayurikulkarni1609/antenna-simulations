9131d3e220cd689d593a2b40fa4a4692b0f45d74
Mayuri Kulkarni <mayurikulkarni1609@gmail.com>
2026-05-23 22:48:29 +0530

    Update yagi_sim.m

--- yagi-uda/yagi_sim.m
+++ yagi-uda/yagi_sim.m
@@ -1,4 +1,10 @@
-y = yagiUda()
-show(y)
-pattern(y, 300e6)
-impedance(y, 250e6 : 5e6 : 350e6)
+y = yagiUda();
+figure;
+pattern(y, 300e6);
+title('Yagi-Uda Radiaton Pattern - 300MHz');
+exportgraphics(gcf, 'yagi_pattern.png', 'Resolution',300);
+
+figure;
+impedance(y, 250e6 : 5e6 : 350e6);
+title('Yagi-Uda Impedance');
+exportgraphics(gcf, 'yagi-impedance.png', 'Resolution',300);
